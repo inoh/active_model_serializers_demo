835b1a01c4a2383a9e89d20aaaca9f24bd8c0565
h.inoue <h.inoue@fenrir.co.jp>
2018-03-30 09:59:21 +0900

    postsのAPI追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  def index
+    render json: Post.all, each_serializer: PostsSerializer
+  end
+
   def create
     render json: User.first.posts.create(title: 'json', note: '作り方')
   end
